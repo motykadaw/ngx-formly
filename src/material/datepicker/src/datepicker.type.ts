90fd79989161d932ed5e3a95f20a0963c2b02e73
Dawid Motyka <motykadaw@gmail.com>
2019-09-09 14:36:36 +0200

    add cd

--- src/material/datepicker/src/datepicker.type.ts
+++ src/material/datepicker/src/datepicker.type.ts
@@ -1,13 +1,20 @@
-import { Component, ViewChild, AfterViewInit, TemplateRef } from '@angular/core';
-import { ɵdefineHiddenProp as defineHiddenProp } from '@ngx-formly/core';
-import { FieldType } from '@ngx-formly/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatDatepickerInput } from '@angular/material/datepicker';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  TemplateRef,
+  ChangeDetectorRef
+} from "@angular/core";
+import { ɵdefineHiddenProp as defineHiddenProp } from "@ngx-formly/core";
+import { FieldType } from "@ngx-formly/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatDatepickerInput } from "@angular/material/datepicker";
 
 @Component({
-  selector: 'formly-field-mat-datepicker',
+  selector: "formly-field-mat-datepicker",
   template: `
-    <input matInput
+    <input
+      matInput
       [id]="id"
       [errorStateMatcher]="errorStateMatcher"
       [formControl]="formControl"
@@ -18,39 +25,52 @@ import { MatDatepickerInput } from '@angular/material/datepicker';
       [formlyAttributes]="field"
       [placeholder]="to.placeholder"
       [tabindex]="to.tabindex || 0"
-      [readonly]="to.readonly">
+      [readonly]="to.readonly"
+    />
     <ng-template #datepickerToggle>
       <mat-datepicker-toggle [for]="picker"></mat-datepicker-toggle>
     </ng-template>
-    <mat-datepicker #picker
+    <mat-datepicker
+      #picker
       [color]="to.color"
       [touchUi]="to.datepickerOptions.touchUi"
       [startView]="to.datepickerOptions.startView"
-      [startAt]="to.datepickerOptions.startAt">
+      [startAt]="to.datepickerOptions.startAt"
+    >
     </mat-datepicker>
-  `,
+  `
 })
-export class FormlyDatepickerTypeComponent extends FieldType implements AfterViewInit {
-  @ViewChild(MatInput, <any> { static: true }) formFieldControl!: MatInput;
+export class FormlyDatepickerTypeComponent extends FieldType
+  implements AfterViewInit {
+  @ViewChild(MatInput, <any>{ static: true }) formFieldControl!: MatInput;
   @ViewChild(MatDatepickerInput) datepickerInput!: MatDatepickerInput<any>;
-  @ViewChild('datepickerToggle') datepickerToggle!: TemplateRef<any>;
+  @ViewChild("datepickerToggle") datepickerToggle!: TemplateRef<any>;
 
   defaultOptions = {
     templateOptions: {
       datepickerOptions: {
-        startView: 'month',
-        datepickerTogglePosition: 'suffix',
-      },
-    },
+        startView: "month",
+        datepickerTogglePosition: "suffix"
+      }
+    }
   };
+
+  constructor(private cd: ChangeDetectorRef) {
+    super();
+  }
 
   ngAfterViewInit() {
     super.ngAfterViewInit();
     // temporary fix for https://github.com/angular/material2/issues/6728
-    (<any> this.datepickerInput)._formField = this.formField;
+    (<any>this.datepickerInput)._formField = this.formField;
 
     setTimeout(() => {
-      defineHiddenProp(this.field, '_mat' + this.to.datepickerOptions.datepickerTogglePosition, this.datepickerToggle);
+      defineHiddenProp(
+        this.field,
+        "_mat" + this.to.datepickerOptions.datepickerTogglePosition,
+        this.datepickerToggle
+      );
+      this.cd.detectChanges;
     });
   }
 }
